585425533db373d115efff5a91154c60eab8acd6
vish27 <vishaka@zestmoney.in>
2019-12-27 15:28:07 +0530

    feat: added files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PostForm from './PostForm';
+import AllPost from './AllPost';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="navbar">
+          <h2 className="center">Post it</h2>
+        </div>
+        <PostForm />
+        {(this.props.posts.length > 0) && (
+          <AllPost />
+        )}
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    posts: state
+  }
+};
+
+export default connect(mapStateToProps)(App);
+
